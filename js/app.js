9413f8746b2d0effcbbb5b568f75037675991f02
Ayappa Reddy <aayappareddy@gmail.com>
2018-06-26 15:36:04 +0530

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const options = {};
+  const elems = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const options = {
+    fullWidth: true,
+    indicators: true,
+  };
+  const elems = document.querySelectorAll('.carousel');
+  M.Carousel.init(elems, options);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const options = {
+    interval: 6000,
+  };
+  var elems = document.querySelectorAll('.slider');
+  M.Slider.init(elems, options);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.materialboxed');
+  M.Materialbox.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.scrollspy');
+  M.ScrollSpy.init(elems);
+});
